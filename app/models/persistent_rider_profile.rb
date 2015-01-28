d8b904e05dcce6a5190fef4d46b9b50e459ddf65
AviFoxi <frosenox@gmail.com>
2015-01-28 15:17:39 -0500

    add validation to persistent rider profile, user must be registered for at least one ride

--- app/models/persistent_rider_profile.rb
+++ app/models/persistent_rider_profile.rb
@@ -1,7 +1,16 @@
 class PersistentRiderProfile < ActiveRecord::Base
 	belongs_to :user
+	has_many :rider_year_registrations, through: :user
 
 	validates_associated :user, on: :create
+	validate :has_at_least_one_rider_year_registration
 
+	private
+
+	def has_at_least_one_rider_year_registration
+		unless self.rider_year_registrations.count >= 1
+      errors.add :rider_year_registrations, "Your must be registered for at least one ride to have a persistent profile."
+    end
+	end
 	
 end
