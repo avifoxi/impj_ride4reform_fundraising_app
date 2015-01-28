d5c09b2e92ce5face483e1045349ee1e52958967
AviFoxi <frosenox@gmail.com>
2015-01-28 14:28:39 -0500

    current validations for rider year registrations tested and passing

--- spec/models/rider_year_registration_spec.rb
+++ spec/models/rider_year_registration_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe RiderYearRegistration, :type => :model do
-	let(:ryr) { FactoryGirl.create(:rider_year_registration) }	
+	let(:ryr) { FactoryGirl.build(:rider_year_registration) }	
 
 	it "has a valid factory" do 
 		expect(ryr).to be_an_instance_of(RiderYearRegistration)
@@ -11,6 +11,23 @@ RSpec.describe RiderYearRegistration, :type => :model do
 		expect( ryr.update_attributes(goal: 50) ).to eq(false)
 	end
 
+	it "validates user agrees to terms before creation" do 
+		disagree = FactoryGirl.build(:rider_year_registration)
+		disagree.agree_to_terms = false
+		expect(disagree.save).to eq(false)
+	end 
 
+	it "validates user may not create multiple registrations for one ride year" do
+		expect(ryr.save).to eq(true)
+		attrs = attributes_for(:rider_year_registration)
+		double = ryr.user.rider_year_registrations.new(attrs)
+		expect(double.save).to eq(false)
+	end
+
+	it "validates only valid ride options may be submitted" do 
+		invalid_ride_option = FactoryGirl.build(:rider_year_registration)
+		invalid_ride_option.ride_option = 'navel gazing'
+		expect(invalid_ride_option.save).to eq(false)
+	end
 
 end
