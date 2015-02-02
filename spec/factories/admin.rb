d38352454c07d324b3e9cda832d7bd2d19dfe195
AviFoxi <frosenox@gmail.com>
2015-02-02 11:28:56 -0500

    passing model tests with devise installed for users + admins

--- spec/factories/admin.rb
+++ spec/factories/admin.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :admin do
-  	
+  	password Devise.friendly_token.first(8)
+  	email 'admin@test.com'
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,20 @@
 FactoryGirl.define do
   factory :user do
 
+  	password Faker::Internet.password
+  	
     trait :rider do
 	    first_name "Rider"
 			last_name "Test"
 			email "rider@test.com"
+			
 	  end
 
 	  trait :donor do
 	    first_name "Donor"
 			last_name "Test"
 			email "donor@test.com"
+			
 	  end
 
   end
