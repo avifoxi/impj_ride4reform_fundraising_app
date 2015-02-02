cab488737b422fdec122ec4b014bdd1d4d50c293
AviFoxi <frosenox@gmail.com>
2015-02-02 11:12:07 -0500

    add notices to layout, configure mailer to localhost in dev env, add root_url, gen users controller, add devise views, unmodified

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # for devise - must set root to something
+  root to: "users#index"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
