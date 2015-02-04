c3d488163d1aa185fb7b4f4b0b7f592728444b59
AviFoxi <frosenox@gmail.com>
2015-02-04 10:28:07 -0500

    reconfigure database cleaner to run between tests, less headache. remove thoughtbots recommended config.

--- spec/controllers/admin/admins_controller_spec.rb
+++ spec/controllers/admin/admins_controller_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Admin::AdminsController, :type => :controller do
-	# login_admin
-
+	let(:admin) { FactoryGirl.create(:admin) }	
+	# login_admin(FactoryGirl.create(:admin))
+	
 	it 'redirects non logged in user to admin sign in page' do 
 		get :index
 		expect(response).to redirect_to(new_admin_session_path)
 	end
 
-	
+	it 'allows logged in admin to get new admin template' do
+		# login_admin(admin)
+		sign_in admin
+		get :new
+		expect(response).to render_template(:new)
+	end
 
 end
