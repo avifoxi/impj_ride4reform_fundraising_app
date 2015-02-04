9b6c919650a8841f3800b4b6039af9a1b68e7d93
AviFoxi <frosenox@gmail.com>
2015-02-04 10:07:05 -0500

    first admins controller test pass

--- spec/controllers/admin/admins_controller_spec.rb
+++ spec/controllers/admin/admins_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Admin::AdminsController, :type => :controller do
-	login_admin
+	# login_admin
+
+	it 'redirects non logged in user to admin sign in page' do 
+		get :index
+		expect(response).to redirect_to(new_admin_session_path)
+	end
+
+	
 
 end
