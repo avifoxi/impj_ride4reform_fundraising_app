add055b2ce2a9306b2cf08519f9111bc9d822b58
AviFoxi <frosenox@gmail.com>
2015-02-12 16:30:14 -0500

    showing errors problems

--- app/controllers/rider_year_registrations_controller.rb
+++ app/controllers/rider_year_registrations_controller.rb
@@ -21,20 +21,53 @@ class RiderYearRegistrationsController < ApplicationController
 		p "#{ryr_params.inspect}"
 		p "#"*80
 
-		
+		@ryr = RiderYearRegistration.new(user: current_user)
+		if @ryr.update_attributes(ryr_params)
+
+			p "#"*80
+			p 'ryr'
+			p "#{@ryr.inspect}"
+			p "#"*80
+		else 
+			p "#{ @ryr.errors.full_messages.to_sentence.inspect}" 
+			p "#"*80
+			p 'ryr errors'
+			p "#{@ryr.errors }"
+			p "#"*80
+			@ryr.build_user
+			@ryr.mailing_addresses.build(mailing_addess_params)
+			@ryr.user.build_persistent_rider_profile(per_params)
+			@errors = @ryr.errors
+			render :new
+			# redirect_to new_rider_year_registration_path, :flash => { :errors => @ryr.errors }
+		end
+
+			# p "#"*80
+			# p 'ryr'
+			# p "#{@ryr.inspect}"
+			# p "#"*80
+
 	end
 
 	private 
 
 	def ryr_params
-    params.require(:rider_year_registration).permit(:ride_option, :goal, user_attributes: [
-    		:mailing_address => [
+    params.require(:rider_year_registration).permit(:ride_option, :goal, 
+    	:mailing_addresses_attributes => [
     			:line_1, :line_2, :city, :state, :zip
-    		], 
-    		:persistent_rider_profile => [
+    		],
+    	:persistent_rider_profile_attributes => [
     			:primary_phone, :secondary_phone, :birthdate, :bio
-    		]
-    	])
+    		] )
   end
+
+  def mailing_addess_params
+  	ryr_params[:mailing_address_attributes]
+  end
+
+  def per_params
+  	ryr_params[:persistent_rider_profile_attributes]
+  end
+
 
 end
--- app/models/rider_year_registration.rb
+++ app/models/rider_year_registration.rb
@@ -3,7 +3,14 @@ class RiderYearRegistration < ActiveRecord::Base
   belongs_to :user
 
   has_many :mailing_addresses, through: :user
+  accepts_nested_attributes_for :mailing_addresses
+  delegate :mailing_addresses, to: :user
+
+
   has_one :persistent_rider_profile, through: :user
+  accepts_nested_attributes_for :persistent_rider_profile
+  delegate :persistent_rider_profile, to: :user
+
 
   accepts_nested_attributes_for :user
 
@@ -14,7 +21,7 @@ class RiderYearRegistration < ActiveRecord::Base
   RIDE_OPTIONS = ['Original Track', 'Light Track', 'Hiking', 'Combination Hiking/Riding']
 
   validate :goal_meets_min_for_ride_year  
-  validates_presence_of :agree_to_terms, on: :create, :message => 'You must accept the terms of the ride to register.'
+  # validates_presence_of :agree_to_terms, on: :create, :message => 'You must accept the terms of the ride to register.'
   validates_associated :user, on: :create
   validates_uniqueness_of :user, scope: :ride_year, :message => 'You may only register once per ride year. Have you already registered?'
   validates :ride_option, inclusion: { in: RIDE_OPTIONS }
