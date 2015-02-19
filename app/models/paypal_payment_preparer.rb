b7eff14e01131782a31556196118b6fd19dfb126
AviFoxi <frosenox@gmail.com>
2015-02-19 13:09:28 -0500

    paypal hash maker

---
+++ app/models/paypal_payment_preparer.rb
@@ -0,0 +1,103 @@
+# require 'paypal-sdk-rest'
+# include PayPal::SDK::REST
+
+class PaypalPaymentPreparer
+
+  attr_reader :payment_hash
+  
+  def initialize(params)
+    @user = params[:user]
+    @cc_info = params[:cc_info]
+    @billing_address = params[:billing_address]
+    @transaction_details = params[:transaction_details]
+    @payment_hash = {}
+    prepare_payment_hash
+  end
+
+  def prepare_payment_hash
+    @payment_hash = {
+      :intent => "sale",
+      :payer => {
+        :payment_method => "credit_card",
+        :funding_instruments => [{
+          :credit_card => {
+            :type => @cc_info['type'],
+            :number => @cc_info['number'],
+            :expire_month => @cc_info['expire_month'],
+            :expire_year => @cc_info['expire_year'],
+            :cvv2 => @cc_info['expire_month'],
+            :first_name => @user.first_name,
+            :last_name => @user.last_name,
+            :billing_address => {
+              :line1 => @billing_address.line_1,
+              :city => @billing_address.city,
+              :state => @billing_address.state,
+              :postal_code => @billing_address.zip,
+              :country_code => "US" }}}]},
+      :transactions => [{
+        :item_list => {
+          :items => [{
+            :name => @transaction_details['name'],
+            # :sku => "item",
+            :price => @transaction_details['amount'],
+            :currency => "USD",
+            :quantity => 1 
+          }]},
+        :amount => {
+          :total => @transaction_details['amount'],
+          :currency => "USD" },
+        :description =>  @transaction_details['description'] }
+      ]
+    }
+    @payment_hash
+  end
+
+end
+
+# PaypalPaymentPreparer.new({user: User.first, billing_address: MailingAddress.first, cc_info: cc_info, transaction_details: transaction_details })
+
+# transaction_details = {'name' => 'rider registration fee', 'amount' => 650, 'description' => 'rider registration fee'}
+
+
+# cc_info = {'type' => 'visa', 'number'=> '123', 'expire_month' => '123', 'expire_year' => '2016'}
+
+
+# # Build Payment object
+# @payment = Payment.new({
+#   :intent => "sale",
+#   :payer => {
+#     :payment_method => "credit_card",
+#     :funding_instruments => [{
+#       :credit_card => {
+#         :type => "visa",
+#         :number => "4417119669820331",
+#         :expire_month => "11",
+#         :expire_year => "2018",
+#         :cvv2 => "874",
+#         :first_name => "Joe",
+#         :last_name => "Shopper",
+#         :billing_address => {
+#           :line1 => "52 N Main ST",
+#           :city => "Johnstown",
+#           :state => "OH",
+#           :postal_code => "43210",
+#           :country_code => "US" }}}]},
+#   :transactions => [{
+#     :item_list => {
+#       :items => [{
+#         :name => "item",
+#         :sku => "item",
+#         :price => "1",
+#         :currency => "USD",
+#         :quantity => 1 }]},
+#     :amount => {
+#       :total => "1.00",
+#       :currency => "USD" },
+#     :description => "This is the payment transaction description." }]})
+
+# Create Payment and return the status(true or false)
+# if @payment.create
+#   @payment.id     # Payment Id
+# else
+#   @payment.error  # Error Hash
+# end
