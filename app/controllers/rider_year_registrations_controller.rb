2e3503617b804a479f1b1e4ef108233debb59050
AviFoxi <frosenox@gmail.com>
2015-02-18 22:14:58 -0500

    creating mailing addy

--- app/controllers/rider_year_registrations_controller.rb
+++ app/controllers/rider_year_registrations_controller.rb
@@ -5,7 +5,6 @@ class RiderYearRegistrationsController < ApplicationController
 	def new 
 		## assume never registered before - brand new ... and deal with alternative scenario once this is built
 		@ryr = RiderYearRegistration.new
-
 	end
 
 	def create 
@@ -51,14 +50,31 @@ class RiderYearRegistrationsController < ApplicationController
 
 	end
 
-
 	def new_mailing_address
 		@ryr = RiderYearRegistration.find(params[:rider_year_registration])
 		@ryr.mailing_addresses.build
 	end
 
 	def create_mailing_address
+		p '$'*80
+		p 'malin addinges params'
+		p "#{mailing_addesses_params}"
+		p '$'*80
 
+		@ryr = RiderYearRegistration.find(params[:ryr_id])
+		m_a = @ryr.mailing_addresses.build(user: @ryr.user)
+
+		if m_a.update_attributes(mailing_addesses_params)
+			p '$'*80
+			p 'mailing addy saved to db'
+			p "#{m_a.inspect}"
+			p '$'*80
+		else
+			@errors = m_a.errors
+			render :new_mailing_address
+		end
+
+		
 	end
 
 	
@@ -84,7 +100,7 @@ class RiderYearRegistrationsController < ApplicationController
   end
 
   def mailing_addesses_params
-  	full_params['mailing_addresses_attributes']
+  	full_params['mailing_addresses_attributes']['0']
   end
 
   def prp_params
