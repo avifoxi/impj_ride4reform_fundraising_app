9b419f959a8e294d7248880342cae2a2ed3caa34
AviFoxi <frosenox@gmail.com>
2015-03-30 21:20:54 -0400

    cleaning up mailer functions -- moving donation flow next

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -1,12 +1,12 @@
 class DonationMailer < ApplicationMailer
   default from: "donations@ride4reform.com"
 
-  def sample_email(donation)
-  	@donation = donation
-  	@donor = @donation.user
-  	@rider = @donation.rider
-  	mail(to: @donor.email, subject: 'foo bar baz?')
-  end
+  # def sample_email(donation)
+  # 	@donation = donation
+  # 	@donor = @donation.user
+  # 	@rider = @donation.rider
+  # 	mail(to: @donor.email, subject: 'foo bar baz?')
+  # end
 
   def successful_donation_alert_rider(donation)
   	@donation = donation
--- test/mailers/previews/donation_mailer_preview.rb
+++ test/mailers/previews/donation_mailer_preview.rb
@@ -1,7 +1,7 @@
 class DonationMailerPreview < ActionMailer::Preview
-  def sample_mail_preview
-    DonationMailer.sample_email(Donation.first)
-  end
+  # def sample_mail_preview
+  #   DonationMailer.sample_email(Donation.first)
+  # end
 
   def successful_donation_alert_rider_preview
   	DonationMailer.successful_donation_alert_rider(Donation.first)
