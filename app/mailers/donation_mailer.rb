bfbbb0b271d2e8da2f43068978bcf698c2b93d71
AviFoxi <frosenox@gmail.com>
2015-03-30 23:09:49 -0400

    stuck with action mailer configure, alerting donor. no worky, while alerting rider recipient works fine.

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -1,12 +1,12 @@
 class DonationMailer < ApplicationMailer
   default from: "donations@ride4reform.com"
 
-  # def sample_email(donation)
-  # 	@donation = donation
-  # 	@donor = @donation.user
-  # 	@rider = @donation.rider
-  # 	mail(to: @donor.email, subject: 'foo bar baz?')
-  # end
+  def sample_email(donation)
+  	@donation = donation
+  	@donor = @donation.user
+  	@rider = @donation.rider
+  	mail(to: @donor.email, subject: 'foo bar baz?')
+  end
 
   def successful_donation_alert_rider(donation)
   	@donation = donation
@@ -14,15 +14,7 @@ class DonationMailer < ApplicationMailer
   	@rider = @donation.rider
   	@prp = @rider.persistent_rider_profile
   	@percent_of_goal = @prp.delegate_ryr_method(RideYear.current, 'percent_of_goal')
-    # subject: "Donation received from #{@donor.full_name}"
-  	mail(to: @donor.email) do |format|
-      format.html do
-        string = render_to_string("successful_donation_alert_rider")
-        p '#'*80
-        p "#{string.inspect}"
-        return string
-      end
-    end
+  	mail(to: @donor.email, subject: "Donation received from #{@donor.full_name}")
   end
 
   def successful_donation_thank_donor(donation)
