5e1eba7d713ebfc84f1391048018909e6c9e08e8
AviFoxi <frosenox@gmail.com>
2015-04-02 09:30:48 -0400

    ajaxing form submission - ok. now gotta display errors, etc etc

--- app/controllers/rider_year_registrations_controller.rb
+++ app/controllers/rider_year_registrations_controller.rb
@@ -42,8 +42,8 @@ class RiderYearRegistrationsController < ApplicationController
 		prp = @ryr.user.build_persistent_rider_profile(user: @ryr.user)
 
 		if prp.update_attributes(prp_params)
-			user_register_for_ride_before_payment
-			RiderYearRegistrationsMailer.successful_registration_welcome_rider(@ryr)
+			# user_register_for_ride_before_payment
+			# RiderYearRegistrationsMailer.successful_registration_welcome_rider(@ryr)
 
 			redirect_to rider_year_registrations_mailing_address_path(rider_year_registration: @ryr)
 		else
@@ -78,59 +78,79 @@ class RiderYearRegistrationsController < ApplicationController
 	end
 
 	def create_pay_reg_fee
+		p 'params ajax'
+		p "#{params.inspect}"
 
 		@ryr = RiderYearRegistration.find(params[:ryr_id])
 
-		unless cc_info
-
-			@payment_errors = ['Please enter your full credit card information to complete your registration']
-			@mailing_addresses = @ryr.mailing_addresses
-			@custom_billing_address = MailingAddress.new
-			@registration_fee = current_fee
-			render :new_pay_reg_fee
+		def errors_via_json
+			@errors = @ryr.errors
+			# return errors json
+			if @ryr.user.errors 
+				@ryr.user.errors.each do |k,v|
+					@errors.messages[k.to_sym] = [v]
+				end
+			end
+			render json: {
+				errors: @errors 
+			} 
 			return
 		end
 
-		if full_params['custom_billing_address'] == '1'
-			@custom_billing_address = MailingAddress.new(custom_billing_address)
-			@custom_billing_address.user = current_user
-			unless @custom_billing_address.save
-				@errors = @custom_billing_address.errors
-				@mailing_addresses = @ryr.mailing_addresses
-				render :new_pay_reg_fee
+		if cc_info
+			@ryr.user.cc_type = cc_info['type']
+			@ryr.user.cc_number = cc_info['number']
+			@ryr.user.cc_cvv2 = cc_info['cvv2']
+			unless @ryr.user.valid?
+				errors_via_json
 				return
 			end
-			billing_address = @custom_billing_address
 		else
-			billing_address = MailingAddress.find(full_params['mailing_addresses'])
+			@payment_errors = ['Please enter your full credit card information to complete your registration']
+			errors_via_json
+			return
 		end
+
+		# if full_params['custom_billing_address'] == '1'
+		# 	@custom_billing_address = MailingAddress.new(custom_billing_address)
+		# 	@custom_billing_address.user = current_user
+		# 	unless @custom_billing_address.save
+		# 		@errors = @custom_billing_address.errors
+		# 		@mailing_addresses = @ryr.mailing_addresses
+		# 		render :new_pay_reg_fee
+		# 		return
+		# 	end
+		# 	billing_address = @custom_billing_address
+		# else
+		# 	billing_address = MailingAddress.find(full_params['mailing_addresses'])
+		# end
 	
-		ppp = PaypalPaymentPreparer.new({
-			user: current_user,
-			cc_info: cc_info, 
-			billing_address: billing_address,
-			transaction_details: transaction_details
-		})
+		# ppp = PaypalPaymentPreparer.new({
+		# 	user: current_user,
+		# 	cc_info: cc_info, 
+		# 	billing_address: billing_address,
+		# 	transaction_details: transaction_details
+		# })
 
-		if ppp.create_payment
-			@ryr.update_attributes(registration_payment_receipt:
-				@ryr.create_registration_payment_receipt(user: current_user, amount: current_fee, paypal_id: ppp.payment.id, full_paypal_hash: ppp.payment.to_json)
-			)
-			@rider = current_user.persistent_rider_profile
-			RiderYearRegistrationsMailer.successful_registration_welcome_rider(@ryr).deliver
+		# if ppp.create_payment
+		# 	@ryr.update_attributes(registration_payment_receipt:
+		# 		@ryr.create_registration_payment_receipt(user: current_user, amount: current_fee, paypal_id: ppp.payment.id, full_paypal_hash: ppp.payment.to_json)
+		# 	)
+		# 	@rider = current_user.persistent_rider_profile
+		# 	RiderYearRegistrationsMailer.successful_registration_welcome_rider(@ryr).deliver
 
-			flash[:notice] = "Thank you for registering to ride!"
-			redirect_to persistent_rider_profile_path(@rider)
-		else
-			@payment_errors = ppp.payment.error
-			@mailing_addresses = @ryr.mailing_addresses
-			unless @custom_billing_address
-				@custom_billing_address = MailingAddress.new
-			end
-			@registration_fee = current_fee
+		# 	flash[:notice] = "Thank you for registering to ride!"
+		# 	redirect_to persistent_rider_profile_path(@rider)
+		# else
+		# 	@payment_errors = ppp.payment.error
+		# 	@mailing_addresses = @ryr.mailing_addresses
+		# 	unless @custom_billing_address
+		# 		@custom_billing_address = MailingAddress.new
+		# 	end
+		# 	@registration_fee = current_fee
 
-			render :new_pay_reg_fee
-		end
+		# 	render :new_pay_reg_fee
+		# end
 	end
 
 	private 
