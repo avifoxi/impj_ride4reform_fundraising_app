de91e528a45d265656166cd80bc5cd62bb8aef5e
AviFoxi <frosenox@gmail.com>
2015-04-07 23:42:25 -0400

    in midst of admin user update mehtods... and brain is kaput

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -29,4 +29,38 @@ class Admin::UsersController < ApplicationController
 			@prp = @user.persistent_rider_profile
 		end
 	end
+
+	def update
+				p '$'*80
+		p 'admin_user_params b'
+		p "#{admin_user_params.inspect}"
+
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			redirect_to admin_user_path(@user)
+		else 
+			@errors = @user.errors
+			render 'edit'
+		end
+	end 
+
+	# "{\"utf8\"=>\"✓\", \"_method\"=>\"put\", \"authenticity_token\"=>\"cdEC3bWxuiIncbBZcYM+n+hLJKkPCdNfsmRT7+kXTCE=\", \"user\"=>{\"title\"=>\"Dr\", \"first_name\"=>\"Avi\", \"last_name\"=>\"Rider\", \"email\"=>\"a@a.com\", \"persistent_rider_profile\"=>{\"bio\"=>\"Try to hack the RSS array, maybe it will connect the back-end alarm!\", \"birthdate(1i)\"=>\"1967\", \"birthdate(2i)\"=>\"12\", \"birthdate(3i)\"=>\"16\", \"primary_phone\"=>\"1234567890\", \"secondary_phone\"=>\"\"}}, \"commit\"=>\"Update User\", \"action\"=>\"update\", \"controller\"=>\"admin/users\", \"id\"=>\"1\"}"
+
+	private
+
+	def full_params
+		params.require(:user).permit(:first_name, :title, :last_name, :email,
+			:persistent_rider_profile => [
+  			:primary_phone, :secondary_phone, :avatar, :birthdate, :bio
+  		] 
+		)
+	end
+
+	def user_params
+		full_params.except(:persistent_rider_profile)
+	end
+	
+	def prp_params
+		full_params[:persistent_rider_profile]
+	end
 end
