c70f86e3b82fef812daf95e8e724a19ccf1504c3
AviFoxi <frosenox@gmail.com>
2015-04-08 10:25:12 -0400

    functional link w csv download

--- app/controllers/admin/rider_year_registrations_controller.rb
+++ app/controllers/admin/rider_year_registrations_controller.rb
@@ -3,6 +3,8 @@ class Admin::RiderYearRegistrationsController < ApplicationController
 	skip_before_action :authenticate_user!
 	layout "admins"
 
+	include CSVMaker
+
 	# /admin/ride_years/:ride_year_id/rider_year_registrations
 	# /admin/rider_year_registrations
 	def index
@@ -12,6 +14,11 @@ class Admin::RiderYearRegistrationsController < ApplicationController
 		@avg_raised = @ride_year.avg_raised_by_rider
 		@avg_perc = @ride_year.avg_perc_of_rider_goal_met  
 		@total_raised = @ride_year.total_raised
+
+		respond_to do |format|
+      format.html
+      format.csv { send_data gen_csv(@ryrs, [:full_name, :ride_option, :goal, :raised, :percent_of_goal])  }
+    end
 	end
 
 end
