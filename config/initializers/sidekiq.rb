6ac2c51083733569f1e8d606daea197869d93171
AviFoxi <frosenox@gmail.com>
2015-05-10 10:23:38 -0400

    with database path, perhaps thats the holdup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -34,12 +34,12 @@ end
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDISTOGO_URL"] }
 
-  # database_url = ENV['DATABASE_URL']
-  # if database_url
-  #   ENV['DATABASE_URL'] = "#{database_url}?pool=25"
-  #   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-  #   # Note that as of Rails 4.1 the `establish_connection` method requires
-  #   # the database_url be passed in as an argument. Like this:
-  #   # ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-  # end
+  database_url = ENV['DATABASE_URL']
+  if database_url
+    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
+    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+    # Note that as of Rails 4.1 the `establish_connection` method requires
+    # the database_url be passed in as an argument. Like this:
+    # ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+  end
 end
