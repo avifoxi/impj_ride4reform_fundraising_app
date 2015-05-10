c115a6606e7fa14891e415725122b3cdfe2ee4bc
AviFoxi <frosenox@gmail.com>
2015-05-10 11:02:14 -0400

    adjust database  yml to increase pool, add namespace sidekiq to initializer to sync w unicorn.rb, and try dat shiz

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -28,11 +28,11 @@ ENV["REDISTOGO_URL"] ||= 'redis://localhost:6379/12'
 # end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"] }
+  config.redis = { url: ENV["REDISTOGO_URL"], namespace: 'sidekiq' }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"] }
+  config.redis = { url: ENV["REDISTOGO_URL"], namespace: 'sidekiq' }
 
   database_url = ENV['DATABASE_URL']
   if database_url
