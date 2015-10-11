9557cdf1cd4dd0c4ecc32ba7a26028d7288ecc6d
AviFoxi <frosenox@gmail.com>
2015-10-11 11:53:38 -0400

    new, create w error, index up

--- app/controllers/admin/custom_ride_options_controller.rb
+++ app/controllers/admin/custom_ride_options_controller.rb
@@ -13,25 +13,19 @@ class Admin::CustomRideOptionsController < ApplicationController
 	end
 
 	def create
-		year = RideYear.find( params[:ride_year_id] )
-		option = year.custom_ride_options.build( permitted_params )
-		if option.save
-			# force page reload? yes
+		ride_year
+		@option = @year.custom_ride_options.build( permitted_params )
+		if @option.save
+			redirect_to admin_ride_year_custom_ride_options_path( @year )
 		else
-			# # return form w errors
-			# binding.pry
-			# foo = render_to_string('admin/custom_ride_options/_form', locals: { custom_ride_option: option }), 
-			# custom_ride_option: option,
-			# c_r_o_url:  admin_ride_year_custom_ride_options_path( year )
-			# binding.pry
+			render :new
 		end
-
 	end
 
 	private
 
 	def ride_year
-		RideYear.find( params[:ride_year_id] )
+		@year = RideYear.find( params[:ride_year_id] )
 	end
 
 	def permitted_params
